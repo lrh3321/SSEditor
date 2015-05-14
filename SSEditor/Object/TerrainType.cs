254a6c9b1a224035c9110a50a51dee1cd2824f52
lrh3321 <lrh3321@126.com>
2015-05-14 16:42:15 +0800

    Add new feature

---
+++ SSEditor/Object/TerrainType.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： LRH3321
+ * 日期: 2015-5-14
+ * 时间: 9:00
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+
+namespace SSEditor.Object
+{
+	/// <summary>地形类型</summary>
+	public enum TerrainType
+	{
+		/// <summary>空</summary>
+		Air,
+		/// <summary>平原</summary>
+		Plain,
+		/// <summary>墙</summary>
+		Wall,
+		Forest,Village,Town,Pillar,Sea,Ruins
+		/// <summary>小山</summary>
+		Hill,
+		/// <summary>高山</summary>
+		Mountain,
+		/// <summary>城门</summary>
+		Gate,
+		/// <summary>王座</summary>
+		Throne
+	}
+}
